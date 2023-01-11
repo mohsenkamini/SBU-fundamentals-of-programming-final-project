9d0bb2b5457951a53ddeba744e8a17d148f0538f
mohsenkamini <mohsenkamini@gmail.com>
2023-01-11 18:14:21 +0330

    create_user function is available now

--- lib/mohsen-main-lib.cpp
+++ lib/mohsen-main-lib.cpp
@@ -26,55 +26,47 @@ string print_message_input_string (string message)
 {
   string result;
   cout << message;
-  cin >> result;
+  getline(cin,result);
   cout << endl;
   return result;
 }
 
 user create_user (user* new_user) {
 
-  char admin_state_ans;
 
   //cout <<  "Please Enter your first name: ";
   //cin >> new_user.first_name;
   //cout << endl;
 
   new_user -> first_name=print_message_input_string("Please Enter your first name: ");
-  cout << new_user -> first_name <<endl;
-  //cout <<  "                  last name: ";
-  //cin >> new_user.last_name;
-  //cout << endl;
-  //cout <<  "                  phone number: ";
-  //cin >>  new_user.phone_number;
-  //cout << endl;
-  //  
-  //cout <<  "                  user ID: ";
-  //cin >> new_user.id;
-  //cout << endl;
-  //cout <<  "                  password: ";
-  //cin >> new_user.password; // or could be password_hash
-  //cout << endl;
-  //  
-  //cout <<  "                  major: ";
-  //cin >> new_user.major;
-  //cout << endl;
-  //cout <<  "                  degree level: ";
-  //cin >> new_user.degree_level;
-  //cout << endl;
 
+  new_user -> last_name=print_message_input_string("                  last name: ");
 
-  cout <<  "Great! Do you also want to be an Admin of dining ?[y/n]: ";
-  cin >> admin_state_ans;
-  cout << endl;
+  new_user -> phone_number=print_message_input_string("                  phone number: ");
 
-  //if (admin_state_ans == 'y')
-  //{
-  //  new_user.is_admin=true;
-  //  is_there_any_admins=true;
-  //}
-  //else
-  //  new_user.is_admin=false;
-//
-  //cout << "Your account is successfully created!\n";
+  new_user -> id=print_message_input_string("                  user ID: ");
 
+  new_user -> password=print_message_input_string("                  password: ");
+
+  new_user -> major=print_message_input_string("                  major: ");
+
+  new_user -> degree_level=print_message_input_string("                  degree level: ");
+
+  if (!is_there_any_admins)
+  {
+    char admin_state_ans;
+    cout <<  "Great! Do you also want to be an Admin of dining ?[y/n]: ";
+    cin >> admin_state_ans;
+    cout << endl;
+    if (admin_state_ans == 'y')
+    {
+      new_user -> is_admin=true;
+      is_there_any_admins=true;
+    }
+    else
+      new_user -> is_admin=false;
+  }
+
+  cout << "Your account is successfully created!\n";
+  return *new_user;
 }
--- main.cpp
+++ main.cpp
@@ -9,10 +9,16 @@ int main() {
   //myPrintHelloMake();
   //print_warning("RED HELLO");
 
-  user new_user;
-  create_user(&new_user);
-  cout << new_user.first_name << endl;
-  cout << new_user.last_name;
+  user* new_user=new user;
+  *new_user=create_user(new_user);
+  cout << new_user -> first_name << endl;
+  cout << new_user -> last_name << endl;
+  cout << new_user -> phone_number << endl;
+  cout << new_user -> id << endl;
+  cout << new_user -> password << endl;
+  cout << new_user -> major << endl;
+  cout << new_user -> degree_level << endl;
+  cout << new_user -> is_admin << endl;
 
 
   return(0);
